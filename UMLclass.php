26a17f51466048f46e477f0829b970b89daf9dfa
B00144009RabiaSaeed <B00144009@mytudublin.ie>
2023-11-07 22:02:40 +0000

    Wonder if this will commit from laptop hmm

---
+++ UMLclass.php
@@ -0,0 +1,19 @@
+<?php
+class Member
+{
+    private $name ="";
+    private $totalPoints = "";
+    private totalPoints = "";
+    private totalGames = "";
+    private totalGoals = "";
+    
+    public function __construct(parm1)
+    {
+        
+    }
+        
+    public function finalScore(parm1, parm2)
+    {
+        
+    }
+}
